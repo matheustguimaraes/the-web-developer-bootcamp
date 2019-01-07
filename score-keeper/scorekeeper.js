91853dfc0c35c55450467b7d4588597870a14a02
Matheus Guimarães <matheusguimr@gmail.com>
2019-01-06 23:50:44 -0300

    16.190 Score Keeper Project 2

--- score-keeper/scorekeeper.js
+++ score-keeper/scorekeeper.js
@@ -2,6 +2,10 @@ var p1Button = document.querySelector("#p1");
 var p2Button = document.getElementById("p2");
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
+var resetButton = document.getElementById("reset");
+var numInput = document.querySelector("input")
+var p = document.querySelector("p")
+var winningScoreDisplay = document.querySelector("p span")
 var p1Score = 0;
 var p2Score = 0;
 var gameOver = false;
@@ -11,7 +15,7 @@ p1Button.addEventListener("click", function() {
     if(!gameOver) {
         p1Score++;
         if(p1Score === winningScore) {
-            console.log("GAME OVER!!!");
+            p1Display.classList.add("winner");
             gameOver = true;
         }
         p1Display.textContent = p1Score;
@@ -19,9 +23,33 @@ p1Button.addEventListener("click", function() {
 })
 
 p2Button.addEventListener("click", function() {
-    p2Score++;
-    if(p2Score === winningScore) {
-        gameOver = true;
+    if(!gameOver) {
+        p2Score++;
+        if(p2Score === winningScore) {
+            p2Display.classList.add("winner");
+            gameOver = true;
+        }
+        p2Display.textContent = p2Score;    
     }
-    p2Display.textContent = p2Score;
+})
+
+resetButton.addEventListener("click", function() {
+    reset();
+})
+
+function reset() {
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove("winner");
+    p2Display.classList.remove("winner");
+    gameOver = false;
+}
+
+
+numInput.addEventListener("change", function() {
+    winningScoreDisplay.textContent = this.value;
+    winningScore = Number(this.value);
+    reset();
 })
