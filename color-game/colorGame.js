189b59f6e2f79780f0376fe13835b79f96c61fb8
Matheus Guimarães <matheusguimr@gmail.com>
2019-01-08 19:10:50 -0300

    17.198 Creating the Color Game 4

--- color-game/colorGame.js
+++ color-game/colorGame.js
@@ -1,22 +1,18 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)",
-]
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor(colors);
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i++) {
     // add initial colors to squares
     squares[i].style.backgroundColor = colors[i];
+    console.log(colors[i]);
+    
 
     // add click listeners to squares
     squares[i].addEventListener("click", function() {
@@ -26,6 +22,7 @@ for (var i = 0; i < squares.length; i++) {
         if(clickedColor === pickedColor) {
             messageDisplay.textContent = "Correct";
             changeColors(clickedColor);
+            h1.style.backgroundColor = clickedColor;
         } else {
             this.style.backgroundColor = "#232323";
             messageDisplay.textContent = "Try Again";
@@ -42,6 +39,25 @@ function changeColors(color) {
 }
 
 function pickColor(colors) {
-    var random = Math.floor((Math.random() * colors.length) + 1);
+    var random = Math.floor(Math.random() * colors.length);
     return colors[random];
+}
+
+function generateRandomColors(num) {
+    // make an array
+    var arr = []
+    // add num random colors to array
+    for (var i = 0; i < num; i++) {
+        // get random color and push into arr
+        arr.push(randomColor());
+    }
+    // return that array
+    return arr
+}
+
+function randomColor() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
 }
