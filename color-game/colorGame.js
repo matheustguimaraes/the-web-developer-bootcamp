6ca319b6c107e17c362f8b99fdd1d625be69a693
Matheus Guimarães <matheusguimr@gmail.com>
2019-01-09 23:28:42 -0300

    17.200 Creating the Color Game 6

--- color-game/colorGame.js
+++ color-game/colorGame.js
@@ -1,15 +1,46 @@
-var colors = generateRandomColors(6);
-
+var colors = generateRandomColors(numSquares);
+var numSquares = 6;
 var pickedColor = pickColor();
 var h1 = document.querySelector("h1");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
 var resetButton = document.querySelector("#reset")
 var squares = document.querySelectorAll(".square");
 var messageDisplay = document.querySelector("#message");
 var colorDisplay = document.getElementById("colorDisplay");
 
+easyBtn.addEventListener("click", function() {
+    easyBtn.classList.add("selected");
+    hardBtn.classList.remove("selected");
+    numSquares = 3;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for (let i = 0; i < squares.length; i++) {
+        if (colors[i]) {
+            squares[i].style.backgroundColor = colors[i];
+        } else {
+            squares[i].style.display = "none";
+        }     
+    }
+})
+
+hardBtn.addEventListener("click", function() {
+    hardBtn.classList.add("selected");
+    easyBtn.classList.remove("selected");
+    numSquares = 6;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for (let i = 0; i < squares.length; i++) {
+            squares[i].style.backgroundColor = colors[i];
+            squares[i].style.display = "block";
+    }
+})
+
 resetButton.addEventListener("click", function() {
     // generate all new colors
-    colors = generateRandomColors(6);
+    colors = generateRandomColors(numSquares);
     // pick a new random color from array
     pickedColor = pickColor();
     // change colors of squares
