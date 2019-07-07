c5e189adc4110fb626150ce2fa21dec63abaf092
Matheus Guimaraes <matheusguimr@gmail.com>
2019-07-07 17:28:12 -0300

    Intro to Node.js

---
+++ intro-to-node/a.js
@@ -0,0 +1,13 @@
+var scores = [90, 98, 89, 100, 100, 86, 94];
+average(scores);
+
+var scores2 = [40, 65, 77, 82, 80, 54, 73, 63, 95, 49];
+average(scores2);
+
+function average(numbers) {
+    temp = 0;
+    for (var i = 0, len = numbers.length; i < len; i++) {
+        temp += numbers[i];
+    }
+    console.log(parseInt(temp / numbers.length));
+}
