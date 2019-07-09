944c11be2a6bb22de6b51ad14f79aacc5b88dff0
Matheus Guimaraes <matheusguimr@gmail.com>
2019-07-09 00:15:21 -0300

    Add movie search app

---
+++ movie-search-app/app.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var request = require("request");
+var app = express();
+port = 3000;
+app.set("view engine", "ejs");
+
+
+app.get("/", function(req, res) {
+    res.render("search");
+});
+
+app.get("/results", function(req, res) {
+    var query = req.query.search;
+    var url = "https://www.omdbapi.com/?s=" + query;
+
+    request(url, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var data = JSON.parse(body);
+            res.render("results", {
+                data: data
+            });
+        }
+    });
+});
+
+app.listen(port, function(req, res) {
+    console.log('Movie App has started!!!');
+});
+
